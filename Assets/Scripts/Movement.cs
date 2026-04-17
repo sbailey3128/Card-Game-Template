6cef52713610dda4830ecd0af8eb0e95a538f0ab
sbailey3128 <sbailey3128@student.pps.net>
2026-04-17 12:48:28 -0700

    dih </3

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("pressed space");
+        
+        }
+        if (Input.GetKey)
+    }
+}
